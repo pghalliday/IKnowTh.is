e8df47081a743cb11744146a090d2204cdd81c37
Matt Feigal <matt.feigal@gmail.com>
2012-07-07 14:31:11 +0200

    create port variable for heroku hosting

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.get('/', routes.index);
 app.get('/book', routes.book);
 app.get('/registerExpert', routes.registerExpert);
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
