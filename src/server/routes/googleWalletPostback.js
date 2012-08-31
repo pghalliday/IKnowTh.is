fdfab0abedf8e9e214885a0edd88c4b108ebcad2
Peter Halliday <pghalliday@gmail.com>
2012-08-31 15:18:00 +0200

    Google wallet postback handler now complete

--- src/server/routes/googleWalletPostback.js
+++ src/server/routes/googleWalletPostback.js
@@ -3,11 +3,31 @@ module.exports = function(jwt, User, Event, Payment) {
     var details = jwt.decode(req.body);
     var sellerData = JSON.parse(details.request.sellerData);
     User.findById(sellerData.userId, function(error, user) {
-      Event.findById(sellerData.eventId, function(error, event) {
-        Payment.process(event, user, details, function(error, payment) {
-          res.send(200, details.response.orderId);
-        });
-      });
+      if (error) {
+        res.send(500, error.toString());
+      } else {
+        if (user) {
+          Event.findById(sellerData.eventId, function(error, event) {
+            if (error) {
+              res.send(500, error.toString());
+            } else {
+              if (event) {
+                Payment.process(event, user, details, function(error, payment) {
+                  if (error) {
+                    res.send(500, error.toString());
+                  } else {
+                    res.send(200, details.response.orderId);                  
+                  }
+                });
+              } else {
+                res.send(500, 'Event does not exist');
+              }
+            }
+          });
+        } else {
+          res.send(500, 'User does not exist');
+        }
+      }
     });
   };
 };
--- src/server/routes/googleWalletPostback.test.js
+++ src/server/routes/googleWalletPostback.test.js
@@ -1,68 +1,77 @@
 describe('GoogleWalletPostback', function() {
-  var GoogleWalletPostback = require('./googleWalletPostback.js');
- 
+  var GoogleWalletPostback = require('./googleWalletPostback.js'); 
+
+  // define a mock JWT object
+  var mockJwt = {
+    encode: function(decoded) {
+      return {object: decoded};
+    },
+    decode: function(encoded) {
+      return encoded.object;
+    }
+  };
+
+  // define a mock request object
+  var mockRequest = {
+    body: mockJwt.encode({ 
+      iss: 'Google',
+      aud: '1337133713371337',
+      typ: 'google/payments/inapp/item/v1/postback/buy',
+      iat: '1309988959',
+      exp: '1409988959',
+      request: {
+        name: 'Piece of Cake',
+        description: 'Virtual chocolate cake to fill your virtual tummy',
+        price: '10.50',
+        currencyCode: 'USD',
+        sellerData: '{"userId":123,"eventId":789}'
+      },
+      response: {
+        orderId: '1234567890'
+      }
+    })
+  };
+     
+  // define a mock user table
+  var MockUser = function(_id) {
+    _id.should.eql(123);
+    this._id = _id;
+  };
+
+  // define a mock event table
+  var MockEvent = function(_id) {
+    _id.should.eql(789);
+    this._id = _id;
+  };
+
+  // define a mock payment table
+  var MockPayment = function() {
+  };
+
+  // construct the instance to test
+  var googleWalletPostback = new GoogleWalletPostback(mockJwt, MockUser, MockEvent, MockPayment);      
+      
   describe('#post()', function() {
-    it('should repond with 200 OK and the order ID if the payment can be processed', function(done) {
+    it('should respond with 200 OK and the order ID if the payment can be processed', function(done) {
 
       // flags and stuff so we can verify that the payment was actually processed
       var paymentProcessed = false,
           foundUser,
           foundEvent;
-
-      // define a mock JWT object
-      var mockJwt = {
-        encode: function(decoded) {
-          return {object: decoded};
-        },
-        decode: function(encoded) {
-          return encoded.object;
-        }
-      };
- 
-      // define a mock request object
-      var mockRequest = {
-        body: mockJwt.encode({ 
-          iss: 'Google',
-          aud: '1337133713371337',
-          typ: 'google/payments/inapp/item/v1/postback/buy',
-          iat: '1309988959',
-          exp: '1409988959',
-          request: {
-            name: 'Piece of Cake',
-            description: 'Virtual chocolate cake to fill your virtual tummy',
-            price: '10.50',
-            currencyCode: 'USD',
-            sellerData: '{"userId":123,"eventId":789}'
-          },
-          response: {
-            orderId: '1234567890'
-          }
-        })
-      };
-         
-      // define a mock user table
-      var MockUser = function(_id) {
-        _id.should.eql(123);
-        this._id = _id;
-      };
+      
+      // override the MockUser findById method
       MockUser.findById = function(_id, callback) {
         foundUser = new MockUser(_id);
         callback(null, foundUser);
       };
 
-      // define a mock event table
-      var MockEvent = function(_id) {
-        _id.should.eql(789);
-        this._id = _id;
-      };
+      // override the MockEvent findById method
       MockEvent.findById = function(_id, callback) {
          foundEvent = new MockEvent(_id);
          callback(null, foundEvent);
       };
     
-      // define a mock payment table
-      var MockPayment = function() {
-      };
+      // override the MockPayment process method
       MockPayment.process = function(event, attendee, details, callback) {
         event.should.equal(foundEvent, 'should be processing payment for correct event');
         attendee.should.eql(foundUser, 'should be processing payment for correct attendee');
@@ -81,28 +90,177 @@ describe('GoogleWalletPostback', function() {
         }
       };
       
-      var googleWalletPostback = new GoogleWalletPostback(mockJwt, MockUser, MockEvent, MockPayment);      
       googleWalletPostback.post(mockRequest, mockResponse);
     });
     
-    it('should repond with 500 error and error mesage if the payment cannot be processed as the user is unknown', function(done) {
-      /* TODO */
-      done();
+    it('should respond with 500 error and error mesage if the payment cannot be processed as the user is unknown', function(done) {
+
+      // flags and stuff so we can verify that the payment was actually processed
+      var paymentProcessed = false;
+      
+      // override the MockUser findById method
+      MockUser.findById = function(_id, callback) {
+        callback(null, null);
+      };
+
+      // override the MockEvent findById method
+      MockEvent.findById = function(_id, callback) {
+         callback(null, new MockEvent(_id));
+      };
+    
+      // override the MockPayment process method
+      MockPayment.process = function(event, attendee, details, callback) {
+        paymentProcessed = true;
+        callback(null, null);
+      };
+      
+      // define a mock response object
+      var mockResponse = {
+        send: function(status, body) {
+          paymentProcessed.should.eql(false, 'payment should not have been processed');
+          status.should.eql(500, 'response status should be 500');
+          body.should.eql('User does not exist', 'should have the correct error message');
+          done();
+        }
+      };
+      
+      googleWalletPostback.post(mockRequest, mockResponse);
     });
     
-    it('should repond with 500 error and error mesage if the payment cannot be processed as the event is unknown', function(done) {
-      /* TODO */
-      done();
+    it('should respond with 500 error and error mesage if the payment cannot be processed as an error is encountered while looking up the user', function(done) {
+
+      // flags and stuff so we can verify that the payment was actually processed
+      var paymentProcessed = false;
+      
+      // override the MockUser findById method
+      MockUser.findById = function(_id, callback) {
+        callback(new Error('Error retrieving user'), null);
+      };
+
+      // override the MockEvent findById method
+      MockEvent.findById = function(_id, callback) {
+         callback(null, new MockEvent(_id));
+      };
+    
+      // override the MockPayment process method
+      MockPayment.process = function(event, attendee, details, callback) {
+        paymentProcessed = true;
+        callback(null, null);
+      };
+      
+      // define a mock response object
+      var mockResponse = {
+        send: function(status, body) {
+          paymentProcessed.should.eql(false, 'payment should not have been processed');
+          status.should.eql(500, 'response status should be 500');
+          body.should.eql((new Error('Error retrieving user')).toString(), 'should have the correct error message');
+          done();
+        }
+      };
+      
+      googleWalletPostback.post(mockRequest, mockResponse);
+    });
+        
+    it('should respond with 500 error and error mesage if the payment cannot be processed as the event is unknown', function(done) {
+
+      // flags and stuff so we can verify that the payment was actually processed
+      var paymentProcessed = false;
+      
+      // override the MockUser findById method
+      MockUser.findById = function(_id, callback) {
+        callback(null, new MockUser(_id));
+      };
+
+      // override the MockEvent findById method
+      MockEvent.findById = function(_id, callback) {
+         callback(null, null);
+      };
+    
+      // override the MockPayment process method
+      MockPayment.process = function(event, attendee, details, callback) {
+        paymentProcessed = true;
+        callback(null, null);
+      };
+      
+      // define a mock response object
+      var mockResponse = {
+        send: function(status, body) {
+          paymentProcessed.should.eql(false, 'payment should not have been processed');
+          status.should.eql(500, 'response status should be 500');
+          body.should.eql('Event does not exist', 'should have the correct error message');
+          done();
+        }
+      };
+      
+      googleWalletPostback.post(mockRequest, mockResponse);
+    });
+        
+    it('should respond with 500 error and error mesage if the payment cannot be processed as an error is encountered while looking up the event', function(done) {
+
+      // flags and stuff so we can verify that the payment was actually processed
+      var paymentProcessed = false;
+      
+      // override the MockUser findById method
+      MockUser.findById = function(_id, callback) {
+        callback(null, new MockUser(_id));
+      };
+
+      // override the MockEvent findById method
+      MockEvent.findById = function(_id, callback) {
+         callback(new Error('Error retrieving event'), null);
+      };
+    
+      // override the MockPayment process method
+      MockPayment.process = function(event, attendee, details, callback) {
+        paymentProcessed = true;
+        callback(null, null);
+      };
+      
+      // define a mock response object
+      var mockResponse = {
+        send: function(status, body) {
+          paymentProcessed.should.eql(false, 'payment should not have been processed');
+          status.should.eql(500, 'response status should be 500');
+          body.should.eql((new Error('Error retrieving event')).toString(), 'should have the correct error message');
+          done();
+        }
+      };
+      
+      googleWalletPostback.post(mockRequest, mockResponse);
     });
     
-    it('should repond with 500 error and error mesage if the payment cannot be processed as the user has already paid', function(done) {
-      /* TODO */
-      done();
-    });
+    it('should respond with 500 error and error mesage if the payment cannot be processed as an error is encountered while processing the payment', function(done) {
+
+      // flags and stuff so we can verify that the payment was actually processed
+      var paymentProcessed = false;
+      
+      // override the MockUser findById method
+      MockUser.findById = function(_id, callback) {
+        callback(null, new MockUser(_id));
+      };
+
+      // override the MockEvent findById method
+      MockEvent.findById = function(_id, callback) {
+         callback(null, new MockEvent(_id));
+      };
     
-    it('should repond with 500 error and error mesage if the payment cannot be processed as the user is the host', function(done) {
-      /* TODO */
-      done();
+      // override the MockPayment process method
+      MockPayment.process = function(event, attendee, details, callback) {
+        paymentProcessed = true;
+        callback(new Error('Error processing payment'), null);
+      };
+      
+      // define a mock response object
+      var mockResponse = {
+        send: function(status, body) {
+          paymentProcessed.should.eql(true, 'payment should have been processed');
+          status.should.eql(500, 'response status should be 500');
+          body.should.eql((new Error('Error processing payment')).toString(), 'should have the correct error message');
+          done();
+        }
+      };
+      
+      googleWalletPostback.post(mockRequest, mockResponse);
     });
   });
 });
