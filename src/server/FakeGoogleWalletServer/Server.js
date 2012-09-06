672bf79deac87afc361e3e45bc879ab600d12b57
Peter Halliday <pghalliday@gmail.com>
2012-09-06 17:17:56 +0200

    changed routes to have purchase and purchaseCancel entry points

--- src/server/FakeGoogleWalletServer/Server.js
+++ src/server/FakeGoogleWalletServer/Server.js
@@ -4,8 +4,14 @@ module.exports = function(responder) {
       
   this.app.use(express.bodyParser());
 
-  this.app.post('/', function(req, res) {
-    responder.respond(req.body, function(status, body) {
+  this.app.post('/purchase', function(req, res) {
+    responder.purchase(req.body, function(status, body) {
+      res.send(status, body);
+    });
+  });
+
+  this.app.post('/purchaseCancel', function(req, res) {
+    responder.purchaseCancel(req.body, function(status, body) {
       res.send(status, body);
     });
   });
--- src/server/FakeGoogleWalletServer/Server.test.js
+++ src/server/FakeGoogleWalletServer/Server.test.js
@@ -5,22 +5,40 @@ describe('Server', function() {
 
   var requestBody = {body: 'my request'};
   var responseStatus = 201;
+  var cancelResponseStatus = 501;
   var responseBody = 'my response';
-  var responded = false;
+  var cancelResponseBody = 'my cancel response';
+  var responded;
   
   var responder = {
-    respond: function(data, callback) {
+    purchase: function(data, callback) {
       responded = true;
       data.should.eql(requestBody);
       callback(responseStatus, responseBody);
+    },
+    purchaseCancel: function(data, callback) {
+      responded = true;
+      data.should.eql(requestBody);
+      callback(cancelResponseStatus, cancelResponseBody);
     }
   };
   
   var server = new Server(responder);
 
-  describe('POST /', function() {
-    it('should use respond method to process requests correctly', function(done) {
-      request(server.app).post('/').send(requestBody).expect(responseStatus, responseBody, function(err) {
+  describe('POST /purchase', function() {
+    it('should use purchase method to process requests correctly', function(done) {
+      responded = false;
+      request(server.app).post('/purchase').send(requestBody).expect(responseStatus, responseBody, function(err) {
+        responded.should.eql(true);
+        done(err);
+      });
+    });
+  });
+  
+  describe('POST /purchaseCancelled', function() {
+    it('should use purchaseCancel method to process requests correctly', function(done) {
+      responded = false;
+      request(server.app).post('/purchaseCancel').send(requestBody).expect(cancelResponseStatus, cancelResponseBody, function(err) {
         responded.should.eql(true);
         done(err);
       });
